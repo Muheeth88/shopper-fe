ac219cd84114d77b5422d297be876ea843ae3204
Abdul Muheeth <abdulmuheeth88@gmail.com>
2023-11-25 03:58:35 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import Login_SignUp from './Pages/Login_SignUp';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>} />
+        <Route path='/men' element={<ShopCategory category="men" />} />
+        <Route path='/women' element={<ShopCategory category="women" />} />
+        <Route path='/kids' element={<ShopCategory category="kids" />} />
+        <Route path='/product' element={<Product />}> 
+          <Route path=':productId' element={<Product/>}></Route>
+        </Route>
+        <Route path='/cart' element={<Cart />} />
+        <Route path="/login" element={<Login_SignUp/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
